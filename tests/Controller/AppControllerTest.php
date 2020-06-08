d635185224167dd85bb38acecd5585ea3dc6cdb5
Marin Dauriat <4449227+Marin74@users.noreply.github.com>
2020-06-08 12:02:27 +0200

    Update AppControllerTest.php

--- tests/Controller/AppControllerTest.php
+++ tests/Controller/AppControllerTest.php
@@ -12,10 +12,11 @@ class AppControllerTest extends WebTestCase
     private const METHOD_HEAD = "HEAD";
 
     private const URI_BOOKS = "/books";
-    private const URI_BOOK = "/books/{id}";// TODO PUT method
-    private const URI_NEW_BOOK = "/books/new";// TODO
+    private const URI_BOOK = "/books/{id}";// TODO Manage PUT method
+    private const URI_NEW_BOOK = "/books/new";// TODO Manage this test case
     private const URI_AUTHORS = "/authors";
     private const URI_AUTHOR = "/authors/{id}";
+    private const URI_GENRES = "/genres";
 
     public function testBooksIsUp() {
         $client = static::createClient();
@@ -79,5 +80,81 @@ class AppControllerTest extends WebTestCase
         $client->request(self::METHOD_HEAD, $uri);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testGenresIsUp() {
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, self::URI_GENRES);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testGenresIsUpWithHeadMethod() {
+        $client = static::createClient();
+        $client->request(self::METHOD_HEAD, self::URI_GENRES);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testBooksFormat() {
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, self::URI_BOOKS);
+
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("books", $json);
+        $jsonItems = $json["books"];
+        $this->assertGreaterThan(0, count($jsonItems));
+        $this->assertArrayHasKey("title", $jsonItems[0]);
+    }
+
+    public function testBookFormat() {
+        $uri = str_replace("{id}", 1, self::URI_BOOK);
+
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, $uri);
+
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("book", $json);
+        $jsonItem = $json["book"];
+        $this->assertArrayHasKey("title", $jsonItem);
+    }
+
+    public function testAuthorsFormat() {
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, self::URI_AUTHORS);
+
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("authors", $json);
+        $jsonItems = $json["authors"];
+        $this->assertGreaterThan(0, count($jsonItems));
+        $this->assertArrayHasKey("name", $jsonItems[0]);
+    }
+
+    public function testAuthorFormat() {
+        $uri = str_replace("{id}", 1, self::URI_AUTHOR);
+
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, $uri);
+
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("author", $json);
+        $jsonItem = $json["author"];
+        $this->assertArrayHasKey("name", $jsonItem);
+    }
+
+    public function testGenresFormat() {
+        $client = static::createClient();
+        $client->request(self::METHOD_GET, self::URI_GENRES);
+
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey("genres", $json);
+        $jsonItems = $json["genres"];
+        $this->assertGreaterThan(0, count($jsonItems));
+        $this->assertArrayHasKey("name", $jsonItems[0]);
     }
 }
